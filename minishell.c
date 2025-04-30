901e19f6a6a8132f4e85b7c57ce7782a1bbc1e98
Zoliac <lenypatin@gmail.com>
2025-04-30 04:52:49 +0200

    Partially finished handling the parenthesis, for subprograms handling + added variables. Fix needed : -Functions names -Functions lines limits -Perfectly fixing parenthesis user input handling

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lomont <lomont@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lpatin <lpatin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 21:53:30 by lomont            #+#    #+#             */
-/*   Updated: 2025/04/30 01:50:16 by lomont           ###   ########.fr       */
+/*   Updated: 2025/04/30 03:36:37 by lpatin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int main(void)
 	tokens = malloc(sizeof(t_token));
 	str = readline("caca ");
 	ft_strtok(tokens, str, ' ');
-	// printf("Premier token: %s\n", tokens->tokens[0]);
-	// printf("Deuxieme token: %s\n", tokens->tokens[1]);
-	// printf("Troisieme token: %s\n", tokens->tokens[2]);
-	// printf("Quatrieme token: %s\n", tokens->tokens[3]);
+	printf("Premier token: %s\n", tokens->tokens[0]);
+	printf("Deuxieme token: %s\n", tokens->tokens[1]);
+	printf("Troisieme token: %s\n", tokens->tokens[2]);
+	printf("Quatrieme token: %s\n", tokens->tokens[3]);
 	free_tab(tokens->tokens);
 	free(tokens);
 	return (0);
