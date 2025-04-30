53f0f1c110eb8292b43fb03460eaa9858c82a7d4
Zoliac <lenypatin@gmail.com>
2025-04-30 04:52:49 +0200

    Partially finished handling the parenthesis, for subprograms handling + added variables. Fix needed : -Functions names -Functions lines limits -Perfectly fixing parenthesis user input handling

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -18,16 +18,16 @@ int ft_nb_tokens(char *str)
 	return (k);
 }
 
-void set_caca(char *str, size_t i, t_token *tokens)
+void set_caca(char *str, size_t i, t_token *tokens, size_t depth)
 {
 	size_t k;
 	size_t j;
 
 	j = 0;
 	k = i - 1;
-	if (str[k] == ')')
+	if (str[k] == ')' || depth == 1)
 	{
-		while((str[k] != ' ' || str[k + 1] != '(') && k != 0)
+		while((str[k] != ' ' || str[k + 1] != '(' || depth >= 1) && k != 0)
 			k--;
 	}
 	else
@@ -52,9 +52,11 @@ void	ft_strtok(t_token *tokens, char *str, char delimiter)
 {
 	size_t i;
 	bool parenthesis;
+	size_t depth;
 
 	i = 0;
 	parenthesis = false;
+	depth = 0;
 	tokens->nb_tokens = 0;
 	tokens->tokens = malloc(sizeof(char *) * (ft_nb_tokens(str) + 1));
 	if (!str || !tokens->tokens)
@@ -62,17 +64,22 @@ void	ft_strtok(t_token *tokens, char *str, char delimiter)
 	while (str[i])
 	{
 		if (str[i] == '(')
+		{
 			parenthesis = true;
+			depth++;
+		}
 		else if (str[i] == delimiter && parenthesis == false)
-			set_caca(str, i, tokens);
+			set_caca(str, i, tokens, depth);
 		else if (str[i] == ')' && parenthesis == true)
 		{
-			parenthesis = false;
+			if (depth == 0)
+				parenthesis = false;
+			depth--;
 			i++;
-			set_caca(str, i, tokens);
+			set_caca(str, i, tokens, depth);
 		}
 		i++;
 	}
-	set_caca(str, i, tokens);
+	set_caca(str, i, tokens, depth);
 	tokens->tokens[tokens->nb_tokens] = NULL;
 }
