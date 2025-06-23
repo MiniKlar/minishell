7bc3d74067aa4bd0d0d7898240227625a16f5c57
MiniKlar <omontloris@gmail.com>
2025-06-23 03:12:52 +0200

    - Changed name "shell->fd_in" to "shell->cmd->fd_out". - Moved function "free_cmd_struct" at the end of the function to no free the struct containing the fds before closing them.

--- LIB_SHELL/free/free_shell.c
+++ LIB_SHELL/free/free_shell.c
@@ -6,7 +6,7 @@
 /*   By: miniklar <miniklar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 16:17:06 by lomont            #+#    #+#             */
-/*   Updated: 2025/06/19 02:29:51 by miniklar         ###   ########.fr       */
+/*   Updated: 2025/06/21 02:07:32 by miniklar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ void	free_shell(t_shell *shell)
 {
 	if (!shell)
 		return ;
-	if (shell->cmd)
-		free_cmd_struct(shell->first_cmd);
 	shell->nb_pipe = 0;
-	if (shell->fd_in > -1)
-		close(shell->fd_in);
-	if (shell->fd_out > -1)
-		close(shell->fd_out);
+	if (shell->cmd->fd_in > -1)
+		close(shell->cmd->fd_in);
+	if (shell->cmd->fd_out > -1)
+		close(shell->cmd->fd_out);
 	if (shell->tmp_stdin > -1)
 		close(shell->tmp_stdin);
 	if (shell->tmp_stdout > -1)
 		close(shell->tmp_stdout);
+	if (shell->cmd)
+		free_cmd_struct(shell->first_cmd);
 }
