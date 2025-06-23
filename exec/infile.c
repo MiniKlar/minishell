97e47f648cd768cf722b27b779c634e21d9c113d
MiniKlar <omontloris@gmail.com>
2025-06-23 03:07:21 +0200

    - Changed name "shell->fd_in" by "shell->cmd->fd_in" - Now printing filename in case of error for more precision and understanding.

--- exec/infile.c
+++ exec/infile.c
@@ -9,21 +9,27 @@ bool	redir_infile(t_shell *shell)
 	if (access(str, F_OK) != 0)
 	{
 		perror("File not accessible");
+		ft_putstr_fd(str, 2);
+		ft_putchar_fd('\n', 2);
 		return (false);
 	}
 	else if (access(str, R_OK) != 0)
 	{
 		perror("File not readable");
+		ft_putstr_fd(str, 2);
+		ft_putchar_fd('\n', 2);
 		return (false);
 	}
 	fd = open(str, O_RDWR);
 	if (fd == -1)
 	{
 		perror("Cannot open file");
+		ft_putstr_fd(str, 2);
+		ft_putchar_fd('\n', 2);
 		return (false);
 	}
-	if (shell->fd_in > -1)
-		close(shell->fd_in);
-	shell->fd_in = fd;
+	if (shell->cmd->fd_in > -1)
+		close(shell->cmd->fd_in);
+	shell->cmd->fd_in = fd;
 	return (true);
 }
